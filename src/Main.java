df78d25b73d39cbba59a989b732526570b341c0e
Ivan Borisov <ivan_borisov_88@vk.com>
2022-12-28 16:51:40 +0400

    change

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         int check = 100;
-        int rep = 1100;
+        int rep = 1255;
         check = check + rep;
-        int bonus = rep/100;
-        if (rep>1000) {check = check + bonus;}
+        int bonus = 0;
+        if (rep>1000) {
+            bonus = rep/100;
+            check = check + bonus;
+        }
 
         System.out.println("Итоговый счет: " + check);
         System.out.println("Количество бонусных рублей: " + bonus);
